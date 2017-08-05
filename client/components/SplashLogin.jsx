a5f4cdc593a7871ba7c267b00b8ed3164ddedc95
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-05 15:45:20 -0500

    committing splash login component

--- client/components/SplashLogin.jsx
+++ client/components/SplashLogin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {Grid, Col} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 class SplashLogin extends React.Component {
 	constructor(props) {
@@ -11,14 +12,30 @@ class SplashLogin extends React.Component {
 		// send to backend so we can authorize user
 		// should receive back token?
 		// pass back token so we can send to app to send to dashboard
+		console.log('hello')
 	}
 
 	render() {
 		return (
 			<div className="splash">
+				<div className="logo">
+					<img className="logoImage" src={"/static/images/logo.png"} />
+				</div>
 				<h1 className="appName">Concreet</h1>
 				<h4 className="tagline">Never miss a meeting again</h4>
-				<a onClick={this.handleLogin.bind(this)}>Sign in with google</a>
+				<a href="/auth/google"><button className="signinButton">Sign in with Google</button></a>
+				<div>
+					<img className="meetingImage" src={"/static/images/meeting1.jpg"} />
+				</div>
+				<div>
+					<img className="meetingImage" src={"/static/images/meeting2.jpeg"} />
+				</div>
+				<div>
+					<img className="meetingImage" src={"/static/images/meeting3.jpg"} />
+				</div>
+				<div>
+					<img className="meetingImage" src={"/static/images/meeting4.jpg"} />
+				</div>
 			</div>
 		)
 	}
