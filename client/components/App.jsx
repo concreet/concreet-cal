54bac0d177b7a1845f348e69f71e33f3727b4b3e
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-07 13:12:11 -0500

    Application properly redirects on a successful user signin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Dashboard from './Dashboard.jsx';
 import SplashLogin from './SplashLogin.jsx';
 import * as UserModel from '../models/user.js';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,8 +13,20 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     //functions in here will be invoked when App initiate
+    $.ajax({
+      type: "GET",
+      url: '/session',
+      contentType: 'application/json',
+      success: (data) => {
+        if (data.passport) {
+          this.setState({
+            signedIn: true
+          })
+        }
+      }
+    })
   }
           // {this.state.signedIn && <Dashboard></Dashboard>}
           // {!this.state.signedIn && <SplashLogin></SplashLogin>}
