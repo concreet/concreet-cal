0cfa4f0e6ab0b91ff85ac0e63920666a5a35709c
Ben Childs <benjamin.t.childs@gmail.com>
2017-08-07 23:53:16 -0500

    Schema changes, login creates/finds user and groups

--- server/passport.js
+++ server/passport.js
@@ -2,6 +2,8 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var googleConfig = require('./passportConfig.js');
 var User = require('../db/user.js');
+var Group = require('../db/group.js');
+
 
 passport.serializeUser( (user, done) => {
   done(null, user);
@@ -19,17 +21,29 @@ passport.use(new GoogleStrategy({
 
 	function(token, tokenSecret, profile, done) {
 	 process.nextTick( () => {
+	 	//return token and google profile
 	 		var auth = {
 	 			token: token,
 	 			profile: profile,
 	 		};
-
-      return done(null, auth);
+      User.findOrCreate({ googleId: profile.id }, {isSignedUp: true}, function (err, user) {
+      	console.log('user created or found');
+      	//return actual user record
+      	auth.user = user;
+      	//find contact list too
+      	Group.findOrCreate({owner_id: user._id, isContactList: true}, {group_name: 'Contact List', isContactList: true})
+      	.then((contactList) => {
+      		//return user's contact list
+      		auth.contactList = contactList;
+      	})
+      	.then( Group.find({owner_id: user._id, isContactList: false}) )
+      	.then( (groups) => {
+      		//return user's Groups
+      		auth.groups = groups;
+	      	return done(null, auth);
+      	});
+      });
     });
-      // console.log('hello, steve');
-      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      //   return done(err, profile);
-      // });
   }
 ));
 
