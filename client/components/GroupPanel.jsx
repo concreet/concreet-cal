062e46f5442a7f43fdf1ba2d555d1ae77a52657e
Lina C Yang <nana@Linas-MacBook-Air.local>
2017-08-09 13:15:17 -0500

    Finish all the select states and rendering of all sidebar components

--- client/components/GroupPanel.jsx
+++ client/components/GroupPanel.jsx
@@ -1,20 +1,77 @@
 import React from 'react';
 import GroupPanelEntry from './GroupPanelEntry.jsx';
+import ContactEntry from './ContactEntry.jsx';
+import $ from 'jquery';
 
 class GroupPanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      addContact: false,
+      addGroup: false,
+      addContactToGroup: false
     }
     //binding functions here
+    this.handleAddContact = this.handleAddContact.bind(this);
+    this.handleAddGroup = this.handleAddGroup.bind(this);
+    this.willAddContact = this.willAddContact.bind(this);
+    this.willAddGroup = this.willAddGroup.bind(this);
+    // this.handleUpdateGroup = this.handleUpdateGroup.bind(this);
   }
+
+  willAddContact() {
+    this.setState({
+      addContact: !this.state.addContact      
+    })
+  }
+
+  willAddGroup() {
+    this.setState({
+      addGroup: !this.state.addGroup
+    })
+  }
+
+  handleAddGroup(groupname) {
+    this.props.addGroup(groupname);
+    console.log('cant add a group yet', groupname)
+  }
+
+  handleAddContact(contactmail) {
+    this.props.addContact(contactmail);
+    console.log('cant add a contact yet', contactmail)
+  }
+
+  // handleUpdateGroup() {
+  //   this.props.clearSelectedContacts();
+
+
+  //   this.props.updateGroup()
+  // }
+
+          // Contact First Name: <input className="contactfirst" type="text" />
+          // Contact Last Name: <input className="contactlast" type="text" />
 
   render() {
     return (
       <div className="grouppanel">
-        Proof of group panel
-        <GroupPanelEntry />
+        { this.props.isContactList && <h3> Contact List </h3>}
+        { this.props.isContactList && <button onClick={this.willAddContact}> Add Contacts </button>}
+        { this.state.addContact && <form> 
+          Contact Gmail: <input className="contactmail" type="text" />
+          <input type="submit" value="Submit Gmail" onClick={()=>{ this.handleAddContact($('.contactmail').val()) }} />
+          </form>}
+        { this.props.isContactList && this.props.contacts.map((contact) => <ContactEntry contact={contact} selectContact={this.props.selectContact} selectedContacts={this.props.selectedContacts}/>) }
+
+
+
+        { !this.props.isContactList && <h3> All Group List </h3>}
+        { !this.props.isContactList && 
+          <button onClick={this.willAddGroup}> Add Groups </button> }
+        { this.state.addGroup && <form> 
+          Group Name: <input className="groupname" type="text" />
+          <input type="submit" value="Submit Group Name" onClick={()=>{ this.handleAddGroup($('.groupname').val())}}/>
+          </form>}
+        { !this.props.isContactList && this.props.groups.map((group) => <GroupPanelEntry group={group} selectContact={this.props.selectContact} selectGroup={this.props.selectGroup} updateGroup={this.props.updateGroup}/>)}
       </div>
 
     );
--- client/components/SidePanel.jsx
+++ client/components/SidePanel.jsx
@@ -5,19 +5,28 @@ import ContactEntry from './ContactEntry.jsx';
 class SidePanel extends React.Component {
   constructor(props) {
     super(props);
-    console.log('Props in constructor: ', props);
+    // console.log('Props in constructor: ', props);
     this.state = {
+      // showSubmit: false
 
     }
     //binding functions here
   }
+
+  // showSubmit() {
+  //   this.setState({
+  //     showSubmit: true
+  //   })
+  // }
         // {this.props.contacts.map(contact => <ContactEntry contact={contact} selectContact={this.props.selectContact}/>)}
+        // { this.props.contacts.map((contact) => <ContactEntry contact={contact} selectContact={this.props.selectContact}/>) }
   render() {
+
     return (
       <div className="sidepanel">
-        Proof of side panel
-        {  this.props.contacts && this.props.contacts.map((contact) => <ContactEntry contact={contact} selectContact={this.props.selectContact}/>) }
-        <GroupPanel />
+        Contact and Group Lists
+        <GroupPanel isContactList="true" contacts={this.props.contacts} selectContact={this.props.selectContact} addContact={this.props.addContact} selectedContacts={this.props.selectedContacts}/>
+        <GroupPanel groups={this.props.groups} selectContact={this.props.selectContact} selectGroup={this.props.selectGroup} addGroup={this.props.addGroup} updateGroup={this.props.updateGroup} clearSelectedContacts={this.props.clearSelectedContacts}/>
       </div>
 
     );
