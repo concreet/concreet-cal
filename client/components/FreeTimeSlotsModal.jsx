1b480f0c85c7d7da9a48448d95f47b357c9d5302
Jordan Hoang <jordan.n.hoang@gmail.com>
2017-08-09 14:21:17 -0500

    took out previous binding

--- client/components/FreeTimeSlotsModal.jsx
+++ client/components/FreeTimeSlotsModal.jsx
@@ -21,10 +21,6 @@ class FreeTimeSlotsModal extends React.Component {
     this.state = {
       modalIsOpen: false
     };
-
-    this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
   }
 
   openModal() {
